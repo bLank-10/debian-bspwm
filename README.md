dfeb23110811bf3adc0ab67bd8b56a0fb8b8c393
bLank-10 <uvulircuu@gmail.com@gmail.com>
2023-05-05 21:23:03 +0530

    new patch

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
+
 # debian-bspwm
+
+Use the following iso:
+https://cdimage.debian.org/cdimage/unofficial/non-free/cd-including-firmware/current/amd64/iso-cd/firmware-11.7.0-amd64-netinst.iso 
+
+## Wifi setup
+
+use ip a to find your wlan interface
+
+```bash
+wpa_passphrase wifi-name wifi-password | sudo tee /etc/wpa_supplicant.conf
+sudo wpa_supplicant -c /etc/wpa_supplicant.conf -i wlp6s0 -B
+sudo dhclient wlp6s0
+```
+
+change from wpa supplicant to nmtui
+```bash
+sudo apt install network-manager network-manager-gnome
+sudo reboot
+```
+Now use nmtui to connect to wifi
+
+
+## Installation
+
+```bash
+sudo apt install git
+git clone https://github.com/bLank-10/debian-bspwm
+cd debian-bspwm
+sudo chmod +x install.sh
+./install
+```
+
+## After installation
+
+use lxappearance to change icon -> papirus-icon-theme  & theme -> dracula
+
+### For virtual box resolution
+
+```bash
+cvt 1920 1080 60
+xrandr --newmode "1920x1080_60.00"  173.00  1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync
+xrandr --addmode Virtual1 "1920x1080_60.00"
+xrandr --output Virtual1 --primary --mode "1920x1080_60.00" --pos 0x0 --rotate normal
+```
--- install.sh
+++ install.sh
@@ -1,8 +1,3 @@
-#ip a
-#wpa_passphrase wifi-name wifi-password | sudo tee /etc/wpa_supplicant.conf
-#sudo wpa_supplicant -c /etc/wpa_supplicant.conf -i wlp6s0 -B
-#sudo dhclient wlp6s0
-#ip a
 
 username=$(id -u -n 1000)
 
@@ -11,7 +6,7 @@ sudo apt update -y && sudo apt upgrade -y
 # Bspwm, sxhkd and polybar deps
 sudo apt install gcc g++ make libxcb-xinerama0-dev libxcb-icccm4-dev libxcb-randr0-dev libxcb-util0-dev libxcb-ewmh-dev libxcb-keysyms1-dev libxcb-shape0-dev build-essential cmake cmake-data pkg-config python3-sphinx python3-packaging libuv1-dev libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev i3-wm libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev -y
 
-sudo apt install wget kitty nitrogen picom dmenu pulseaudio nautilus lxappearance papirus-icon-theme xinit -y
+sudo apt install wget kitty nitrogen dmenu picom pulseaudio nautilus lxappearance papirus-icon-theme blueman xinit -y
 
 
 #Brave Stable
@@ -28,7 +23,6 @@ git clone https://github.com/baskerville/bspwm.git
 git clone https://github.com/baskerville/sxhkd.git
 cd bspwm && make && sudo make install
 cd ../sxhkd && make && sudo make install
-
 
 # bspwm and sxhkd configs
 cd /home/$username/debian-bspwm
@@ -48,7 +42,6 @@ cmake ..
 make -j$(nproc)
 sudo make install
 
-
 # polybar configs
 cd /home/$username/debian-bspwm
 mkdir -p /home/$username/.config/polybar
@@ -61,18 +54,13 @@ sudo chmod +x /home/$username/.config/polybar/launch.sh
 mkdir -p /home/$username/.local/share/fonts
 cp fonts/fantasque_sans_mono.ttf /home/$username/.local/share/fonts/
 cp fonts/material_design_iconic_font.ttf /home/$username/.local/share/fonts/
+cp fonts/FantasqueSansMNerdFontPropo-Regular.ttf /home/$username/.local/share/fonts/
 
 
 # kitty
 mkdir -p /home/$username/.config/kitty
 cp configs/kitty.conf /home/$username/.config/kitty/
 sudo chmod +x /home/$username/.config/kitty/kitty.conf
-
-
-# startup
-cd /home/$username
-mkdir .start && cd .start
-echo "exec bspwm" > start.sh
 
 
 # theme
@@ -87,11 +75,22 @@ sudo apt install -f -y
 sudo apt update -y
 
 
-# network manager & applet
-sudo apt install network-manager network-manager-gnome -y
+# proton
+sudo apt install gnome-shell-extension-appindicator -y
+wget https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.3_all.deb
+echo "c409c819eed60985273e94e575fd5dfd8dd34baef3764fc7356b0f23e25a372c  protonvpn-stable-release_1.0.3_all.deb" | sha256sum --check -
+sudo dpkg -i protonvpn-stable-release_1.0.3_all.deb
+sudo apt update -y
+sudo apt install protonvpn -y
 
-#sudo chmod 777 /home/$username/.config
-echo "use lxappearance to change icon -> papirus-icon-theme  &&  theme -> dracula"
-echo "startx ~/.start/start.sh"
+
+# touchpad
+sudo cp /home/$username/debian-bspwm/configs/40-libinput.conf /etc/X11/xorg.conf.d/
+
+# never sleep
+sudo cp /home/username/debian-bspwm/configs/logind.conf /etc/systemd/
+
+
+echo "terminal settings .bashrc"
 
 printf "\e[1;32mDone! Now if you didn't encountered any error you can reboot.\e[0m"
